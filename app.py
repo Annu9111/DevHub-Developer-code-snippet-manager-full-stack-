e540aaef06ad92fab52f17a331de41057acba583
Annu9111 <aksoni911190@gmail.com>
2026-03-16 16:55:52 +0530

    login

--- app.py
+++ app.py
@@ -1,18 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,session
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "devhubsecret"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database/db.sqlite3")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username=? AND password=?",
+            (username,password)
+        )
+
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            session["user"] = username
+            return redirect("/dashboard")
+        else:
+            return "Invalid login"
+
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        import sqlite3
+        conn = sqlite3.connect("database/db.sqlite3")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO users (username,password) VALUES (?,?)",
+            (username,password)
+        )
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/login")
+
     return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" in session:
+        return render_template("dashboard.html", user=session["user"])
+    return redirect("/login")
 
 if __name__ == "__main__":
     app.run(debug=True)
