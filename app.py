85a8cabd1eb312febbdb62f2fb4b4843e65d8f0b
Annu9111 <aksoni911190@gmail.com>
2026-03-17 10:28:05 +0530

    done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,redirect,session
 import sqlite3
 from flask import flash
+import os
 app = Flask(__name__)
 app.secret_key = "devhubsecret"
 
@@ -170,3 +171,8 @@ def logout():
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
